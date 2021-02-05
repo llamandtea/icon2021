35fa3d68240e9af25ef4a54ce9c83a3298eb7abe
llamandtea <lamantea.giuseppe@gmail.com>
2021-02-05 11:55:11 +0100

    Iniziato a implementare la conversione data di arrivo-stagione

--- src/dataset_preprocessing.py
+++ src/dataset_preprocessing.py
@@ -1,12 +1,40 @@
 import pandas as pd
-from datetime import datetime
+import datetime as dt
 
 # Caricamento del dataset in memoria
-data_frame = pd.read_csv("..\hotel_bookings.csv")
+data_frame = pd.read_csv("../hotel_bookings.csv")
 
 # Unione delle serie "children" e "babies"
-data_frame["minors"] = data_frame["children"] + data_frame["babies"]
-data_frame.drop(["children", "babies"])
+data_frame = data_frame.assign(minors=data_frame['children'] + data_frame['babies'])
+# data_frame.drop("children")
+# data_frame.drop("babies")
+
+
+# Conversione delle date di arrivo in stagioni
+# Dizionario da utilizzare per il lookup delle stagioni
+seasons = {
+    "winter": dt.date(dt.MINYEAR, 12, 21),
+    "spring": dt.date(dt.MINYEAR, 3, 20),
+    "summer": dt.date(dt.MINYEAR, 6, 21),
+    "autumn": dt.date(dt.MINYEAR, 9, 22)
+}
+
+# Funzione per convertire la data di arrivo in una riga in una stagione
+def convert_to_season(x):
+    d = dt.datetime.strptime((x["arrival_date_month"] + " " + x["arrival_date_day_of_month"]), "%B %d")
+
+    if seasons["winter"] <= d < seasons["spring"]:
+        return "winter"
+    elif seasons["spring"] <= d < seasons["summmer"]:
+        return "spring"
+    elif seasons["summer"] <= d < seasons["autumn"]:
+        return "summer"
+    else:
+        return "autumn"
+
+
+# Controllare quale parametro passare a convert_to_season
+data_frame = data_frame.assign(season=convert_to_season(data_frame))
 
 # TODO
 """
@@ -18,4 +46,5 @@ Idee per l'implementazione:
 - aggiornamento dei valori sulla tabella
 """
 
-data_frame.to_csv("..\hotel_bookings_updated.csv")
+# Scrittura della tabella in un nuovo file
+# data_frame.to_csv("..\hotel_bookings_updated.csv")
